3be07aa077f0c6b393c2a3dbe7ea22fd42f2aaa5
sudhanshu <sudhanshujoshi2000@gmail.com>
2023-11-11 16:12:53 +0530

    updated with cards

--- src/Components/MainComponent2/main2.js
+++ src/Components/MainComponent2/main2.js
@@ -3,12 +3,92 @@ import React from 'react'
 const Main2 = () => {
   return (
     <>
-    <div style={{"maxHeight":"auto","backgroundColor":"#151515"}}>
-        <div>
-            <h1 class="ml-5" style={{"color":"white,","fontSize":"60px", "display":"inline-block"}}>Our Clients</h1>
+    <div className='container-fluid' style={{"maxHeight":"auto","backgroundColor":"#151515"}}>
+
+      
+
+        <div className=''>
+            <h1 class="ml-5" style={{"color":"white","fontSize":"60px", "display":"inline-block"}}>Our Clients</h1>
             <p className="ml-2" style={{"color":"#FFFFFF","fontSize":"12px", "display":"inline-block"}}>🟧</p>
+            <h4 className="ml-5" style={{"color":"#717171"}}>we've transformed <font color="white">200+</font> visionary ideas into the vibrant realities of <p>ambitious entrepreneurs.</p></h4>
         </div>
+
+        
+        
+
+        <div className='row mt-4 '>
+
+  <div className="col-lg-3 col-md-4 col-sm-12 mb-4 ml-5 mr-3">
+    <div className="card" style={{ backgroundColor: "black" }}>
+      <h4 className="card-header text-center" style={{ color: "white" }}>Amit Sinha</h4>
+      <div className="card-body mt-3 text-center">
+        <h4 className="card-title" style={{ color: "white" }}>CAD Design</h4>
+        <h5 className="card-text" style={{ color: "#C2C0C0" }}>Improving Healthcare delivery using better UX</h5>
+      </div>
     </div>
+  </div>
+
+  <div className="col-lg-3 col-md-4 col-sm-12 mb-4 ml-5 mr-3">
+    <div className="card" style={{ backgroundColor: "#2E2D2D" }}>
+      <h4 className="card-header text-center" style={{ color: "white" }}>Aman Gupta</h4>
+      <div className="card-body mt-3 text-center">
+        <h4 className="card-title" style={{ color: "white" }}>Healthcare</h4>
+        <h5 className="card-text" style={{ color: "#C2C0C0" }}>Improving Healthcare delivery using better UX</h5>
+      </div>
+    </div>
+  </div>
+
+  <div className="col-lg-3 col-md-4 col-sm-12 mb-4 ml-5 mr-3">
+    <div className="card" style={{ backgroundColor: "black" }}>
+      <h4 className="card-header text-center" style={{ color: "white" }}>Sarah Johnson</h4>
+      <div className="card-body mt-3 text-center">
+        <h4 className="card-title" style={{ color: "white" }}>Healthcare</h4>
+        <h5 className="card-text" style={{ color: "#C2C0C0" }}>Improving Healthcare delivery using better UX</h5>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<div className='row mt-5'>
+
+  <div className="col-lg-3 col-md-4 col-sm-12 mb-4 ml-5 mr-3">
+    <div className="card" style={{ backgroundColor: "black" }}>
+      <h4 className="card-header text-center" style={{ color: "white" }}>DR. Kamlesh</h4>
+      <div className="card-body mt-3 text-center">
+        <h4 className="card-title" style={{ color: "white" }}>CAD Design</h4>
+        <h5 className="card-text" style={{ color: "#C2C0C0" }}>Improving Healthcare delivery using better UX</h5>
+      </div>
+    </div>
+  </div>
+
+  <div className="col-lg-3 col-md-4 col-sm-12 mb-4 ml-5 mr-3">
+    <div className="card" style={{ backgroundColor: "#2E2D2D" }}>
+      <h4 className="card-header text-center" style={{ color: "white" }}>Jessica Miller</h4>
+      <div className="card-body mt-3 text-center">
+        <h4 className="card-title" style={{ color: "white" }}>Healthcare</h4>
+        <h5 className="card-text" style={{ color: "#C2C0C0" }}>Improving Healthcare delivery using better UX</h5>
+      </div>
+    </div>
+  </div>
+
+  <div className="col-lg-3 col-md-4 col-sm-12 mb-4 ml-5 mr-3">
+    <div className="card" style={{ backgroundColor: "black" }}>
+      <h4 className="card-header text-center" style={{ color: "white" }}>Amanda Taylor</h4>
+      <div className="card-body mt-3 text-center">
+        <h4 className="card-title" style={{ color: "white" }}>Healthcare</h4>
+        <h5 className="card-text" style={{ color: "#C2C0C0" }}>Improving Healthcare delivery using better UX</h5>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+
+    </div>
+
+   
     </>
   )
 }
